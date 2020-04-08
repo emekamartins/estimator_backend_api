2b04ac1d56361a5cd5b41f35c77ddad6544ac008
chubbystrings <martinsokwor@hotmail.com>
2020-04-08 19:24:03 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+app.use(cors({
+  origin: '*',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+app.options('*', cors());
+
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization header');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const normalizePort = (val) => {
+    const port = Number.parseInt(val, 10);
+  
+    if (typeof (port) !== 'number') { return val; }
+    if (port >= 0) { return port; }
+    return false;
+  };
+  
+  const port = normalizePort(process.env.PORT || '3000');
+  app.set('port', port);
+
+
+
+  app.listen(port, () => {
+    console.log(`Listening on ${port}`);
+  })
