bf9b73aa05a977245953a2ab2f0bad009f9943b1
chubbystrings <martinsokwor@hotmail.com>
2020-04-13 17:25:31 +0100

    response time fix

--- src/middleware/morgan.js
+++ src/middleware/morgan.js
@@ -1,11 +1,27 @@
+/* eslint-disable no-underscore-dangle */
 const morgan = require('morgan');
 const logger = require('../utils/logger');
+
+morgan.token('time', (req, res) => {
+  if (!res._header || !req._startAt) return '';
+  const diff = process.hrtime(req._startAt);
+  let ms = diff[0] * 1e3 + diff[1] * 1e-6;
+
+  // ms = ms.toFixed(3);
+
+  if (+ms < 10) {
+    ms = `0${ms}`;
+  }
+  // const timeLength = 8; // length of final string
+  // format result:
+  return (`${ms}`);
+});
 
 logger.stream = {
   write: (message) => logger.info(message.substring(0, message.lastIndexOf('\n'))),
 };
 
-const data = ':method \t :url \t :status \t :response-time ms';
+const data = ':method \t :url \t :status \t :time ms';
 
 module.exports = morgan(
   data,
