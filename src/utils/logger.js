23974c080a1828ae09b1bb7526b633152ab3d032
chubbystrings <martinsokwor@hotmail.com>
2020-04-13 15:47:50 +0100

    challenge5 logs fix

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,14 +1,10 @@
-const { createLogger, transports, format } = require('winston');
+const { createLogger, transports } = require('winston');
 
 const logger = createLogger({
-  format: format.combine(
-    format.printf((info) => `${info.message}`),
-  ),
   transports: [
     new transports.File({
       filename: './src/app.log',
       maxsize: 5242880,
-      json: false,
       maxFiles: 5,
     }),
   ],
